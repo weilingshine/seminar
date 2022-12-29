6f94a569b4bf640eafbe8185dce41bdce8a35228
weilingshine <weiling.go@gmail.com>
2022-12-29 10:41:57 +0800

    20221229_新增 0309 論壇 & 修正 dropdown menu

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ function rwd_fun3() {
     var width = window.innerWidth;
     var height = window.innerHeight;
     if (width < 992) {
-        $(".nav li a").on('click',function(e){
+        $(".nav li a").off('click').on('click',function(e){
             if($(this).next('.dropdown-menu').css('display') == 'none') {
                 $(this).next().slideDown();
             }else {
